f607ce8e3bb2a391ad3cd3aea79174d80e905a49
Andres Järviste <andresjarviste@gmail.com>
2020-02-14 00:20:09 +0200

    vue buildig back in staging

--- make_stage.sh
+++ make_stage.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#npm run build
+npm run build
 echo 'Generate autoload for php backend'
 ../mangofp/generate_autoload.sh
 echo '*** Staging'
-echo 'Build completed, will stagge generated assets ...'
+echo 'Build completed, will stage generated assets ...'
 cp -v ./dist/js/chunk-vendors.*.js ../mangofp/stage/assets/js/chunk-vendors.js
 cp -v ./dist/js/app.*.js ../mangofp/stage/assets/js/app.js
 cp -v ./dist/css/chunk-vendors.*.css ../mangofp/stage/assets/css/chunk-vendors.css
 cp -v ./dist/css/app.*.css ../mangofp/stage/assets/css/app.css
 echo 'Staging backend'
 cp -rv ../mangofp/src ../mangofp/stage/
-#cp -rv ../mangofp/assets ../mangofp/assets/
 cp -rv ../mangofp/mangofp.php ../mangofp/stage/
 cp -rv ../mangofp/autoload.php ../mangofp/stage/
 echo 'Syncing to the staging server ...'
