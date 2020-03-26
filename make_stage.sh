8a2f48e966228176e8ad7874306b28240e12e159
Andres Järviste <andresjarviste@gmail.com>
2020-03-26 18:33:25 +0200

    fix for staging script for creating missing dirs

--- make_stage.sh
+++ make_stage.sh
@@ -3,6 +3,9 @@ npm run build
 echo 'Generate autoload for php backend'
 echo '*** Staging'
 echo 'Build completed, will stage generated assets ...'
+mkdir -p ../mangofp/stage/assets/js
+mkdir -p ../mangofp/stage/assets/css
+
 cp -v ./dist/js/chunk-vendors.*.js ../mangofp/stage/assets/js/chunk-vendors.js
 cp -v ./dist/js/app.*.js ../mangofp/stage/assets/js/app.js
 cp -v ./dist/css/chunk-vendors.*.css ../mangofp/stage/assets/css/chunk-vendors.css
@@ -13,4 +16,3 @@ echo 'Staging backend'
 echo 'Syncing to the staging server ...'
 rsync -Pav --delete -e "ssh -i $HOME/.ssh/zone_rsa" ../mangofp/stage virt55795@wpkonsult.ee:/data02/virt55795/domeenid/www.wpkonsult.ee/arendus/wp-content/plugins
 echo 'All done!'
- 
