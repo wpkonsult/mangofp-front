3bf2a838cbabd7c731011b6393f21614c294677c
Andres Järviste <andres.jarviste@gmail.com>
2020-09-25 16:35:40 +0300

    staging tweaks

--- stage_backend.sh
+++ stage_backend.sh
@@ -4,5 +4,5 @@ echo '*** Staging only backend'
 ../mangofp/generate_autoload.sh
 ../mangofp/stage_backend.sh
 echo 'Syncing to the staging server ...'
-rsync -Pav --delete -e "ssh -i $HOME/.ssh/zone_rsa" ../mangofp/stage virt55795@wpkonsult.ee:/data02/virt55795/domeenid/www.wpkonsult.ee/arendus/wp-content/plugins
+rsync -Pav --delete -e "ssh -i $HOME/.ssh/zone_rsa" ../mangofp/stage/ virt55795@wpkonsult.ee:/data02/virt55795/domeenid/www.wpkonsult.ee/arendus/wp-content/plugins/mangofp/
 echo 'All done!'
